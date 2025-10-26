7171f970cd0cdb8772aff7e97f44fd0040ca66e2
Kevin Kurt Intong <kevinkurt1422@gmail.com>
2025-10-26 20:49:28 +0800

    Update 1.1
    -added our mission section
    -further spacing adjustments

--- app/page.jsx
+++ app/page.jsx
@@ -2,38 +2,31 @@ import { Button } from "@/components/ui/button";
 import Navbar from "@/components/ui/navbar";
 import { Particles } from "@/components/ui/particles";
 
-
-
 export default function Home() {
   return (
-    <div className="overflow-x-hidden">
-      <Navbar/>
+    <div className="overflow-x-hidden bg-background">
+      <Navbar />
       <main className="relative">
         <section className="relative grid grid-cols-12 mx-16 gap-6 min-h-screen">
-          <div className="flex flex-col col-start-3 col-end-11 items-center justify-center text-center">
+          <div className="flex flex-col col-start-4 col-end-10 items-center justify-center text-center">
             <h1 className="desktop:text-[64px] text-primary font-semibold">
               Give Hope. Fund Care. Change Lives.
             </h1>
-            <h2 className="desktop:text-[20px]">
+            <h2 className="">
               CancerLine Companion connects donors with cancer patients and
               caregivers who need financial support. Every scan brings someone
               closer to healing.
             </h2>
-            <div className="flex flex-row justify-center items-center gap-6 mt-16 ">
-              <Button className="h-[59px] w-[199px] text-[20px] rounded-[12px]">
-                Donate
-              </Button>
-              <Button
-                className="h-[59px] w-[199px] text-[20px] rounded-[12px]"
-                variant="outline"
-              >
+            <div className="flex flex-row justify-center items-center gap-6 mt-8 ">
+              <Button className="w-32">Donate</Button>
+              <Button className="w-32" variant="outline">
                 Learn More
               </Button>
             </div>
           </div>
           <div className="absolute h-[226px] w-[226px] rounded-full border-2 right-[-120] top-20 border-accent-foreground"></div>
-        <div className="absolute h-[355px] w-[355px] rounded-full border-2 right-[-180] top-40 border-accent-foreground"></div>
-        <Particles
+          <div className="absolute h-[355px] w-[355px] rounded-full border-2 right-[-180] top-40 border-accent-foreground"></div>
+          <Particles
             className="absolute inset-0"
             quantity={150}
             ease={60}
@@ -45,8 +38,18 @@ export default function Home() {
           <div className="absolute h-[355px] w-[355px] rounded-full border-2 left-[-180] bottom-40 border-accent-foreground"></div>
           <div className="absolute h-[141px] w-[141px] rounded-full border-2 left-[-90] bottom-20 border-accent-foreground"></div>
         </section>
-        <section>
-          
+        <section className="relative grid grid-cols-12 mx-16 gap-6">
+          <div className="flex flex-col col-start-3 col-end-11 justify-center items-center text-center py-8">
+            <h1 className="text-4xl font-semibold text-primary">Our Mission</h1>
+            <p>
+              CancerLine exists to provide unwavering support, resources, and
+              hope to cancer patients and their families throughout their
+              journey. We believe that no one should face cancer alone, and
+              through our comprehensive programs and community support, we
+              ensure that every step of the way is met with compassion,
+              understanding, and practical assistance.
+            </p>
+          </div>
         </section>
       </main>
     </div>
