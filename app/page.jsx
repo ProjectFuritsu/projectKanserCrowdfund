d1c092dff6e44456571fbef3251dbcd971540639
Kevin Kurt Intong <kevinkurt1422@gmail.com>
2025-10-26 20:39:42 +0800

    Update 1.1
    -added navbar (static)
    -added particle animation in hero

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Navbar from "@/components/ui/navbar";
 import { Particles } from "@/components/ui/particles";
-import Image from "next/image";
+
+
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <Navbar/>
       <main className="relative">
-        <section className="relative grid grid-cols-12 mx-[64px] gap-[24px] min-h-screen">
+        <section className="relative grid grid-cols-12 mx-16 gap-6 min-h-screen">
           <div className="flex flex-col col-start-3 col-end-11 items-center justify-center text-center">
             <h1 className="desktop:text-[64px] text-primary font-semibold">
               Give Hope. Fund Care. Change Lives.
@@ -18,7 +19,7 @@ export default function Home() {
               caregivers who need financial support. Every scan brings someone
               closer to healing.
             </h2>
-            <div className="flex flex-row justify-center items-center gap-[24px] mt-[64px]">
+            <div className="flex flex-row justify-center items-center gap-6 mt-16 ">
               <Button className="h-[59px] w-[199px] text-[20px] rounded-[12px]">
                 Donate
               </Button>
@@ -30,20 +31,23 @@ export default function Home() {
               </Button>
             </div>
           </div>
-        </section>
-        <div className="absolute h-[226px] w-[226px] rounded-full border-2 right-[-120] top-20 border-accent-foreground"></div>
+          <div className="absolute h-[226px] w-[226px] rounded-full border-2 right-[-120] top-20 border-accent-foreground"></div>
         <div className="absolute h-[355px] w-[355px] rounded-full border-2 right-[-180] top-40 border-accent-foreground"></div>
         <Particles
             className="absolute inset-0"
             quantity={150}
             ease={60}
             staticity={30}
-            color="#372AAC"
+            color="#816DFF"
             size={1.2}
           />
           <div className="absolute h-[226px] w-[266px] rounded-full border-2 left-[-200] bottom-100 border-accent-foreground"></div>
           <div className="absolute h-[355px] w-[355px] rounded-full border-2 left-[-180] bottom-40 border-accent-foreground"></div>
           <div className="absolute h-[141px] w-[141px] rounded-full border-2 left-[-90] bottom-20 border-accent-foreground"></div>
+        </section>
+        <section>
+          
+        </section>
       </main>
     </div>
   );
--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -1,10 +1,14 @@
+import { Button } from "./button";
+import Link from 'next/link'
 export default function Navbar() {
   return (
-    <ul className="flex flex-row justify-between items-center">
-      <li>About</li>
-      <li>How it Works</li>
-      <li>Partners</li>
-      <li>Stories</li>
+    <ul className="py-8 flex flex-row justify-center items-center gap-40 fixed w-full mx-auto z-10">
+      <img src="/assets/Logo.svg" className="h-12 w-12"></img>
+      <Link href="">About</Link>
+      <Link href="">How it Works</Link>
+      <Link href="">Partners</Link>
+      <Link href="">Stories</Link>
+      <Button variant="outline">Contact Us</Button>
     </ul>
   );
 }
