568c62e22562c554d26ac180aeef471f0342ec6b
Kevin Kurt Intong <kevinkurt1422@gmail.com>
2025-10-26 18:37:43 +0800

    new entry

---
+++ projectcanlinecrowdfund/app/layout.jsx
@@ -0,0 +1,25 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const metadata = {
+  title: "CancerLine Funding",
+  description: "CancerLine Companion Official Page",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="bg-background text-foreground">
+      <body
+        className={`${poppins.variable} ${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
